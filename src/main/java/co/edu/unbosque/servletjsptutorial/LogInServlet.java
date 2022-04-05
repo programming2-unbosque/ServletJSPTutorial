dde9c2974d3ce103031ef8f419d56d267881b47c
Fabian Peña <fabiancpl91@gmail.com>
2022-04-05 15:04:03 -0500

    Function to create user

--- src/main/java/co/edu/unbosque/servletjsptutorial/LogInServlet.java
+++ src/main/java/co/edu/unbosque/servletjsptutorial/LogInServlet.java
@@ -2,7 +2,6 @@ package co.edu.unbosque.servletjsptutorial;
 
 import java.io.*;
 import java.util.List;
-import java.util.Optional;
 
 import co.edu.unbosque.servletjsptutorial.dtos.User;
 import co.edu.unbosque.servletjsptutorial.services.UserService;
@@ -13,11 +12,8 @@ import jakarta.servlet.annotation.*;
 
 @WebServlet(name = "login", value = "/login")
 public class LogInServlet extends HttpServlet {
-    private String message;
 
-    public void init() {
-        message = "Hello, ";
-    }
+    public void init() {}
 
     public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
         response.setContentType("text/html");
@@ -25,10 +21,10 @@ public class LogInServlet extends HttpServlet {
         String username = request.getParameter("username");
         String password = request.getParameter("password");
 
-        List<User> users = new UserService().getUsers().get();
+        List<User> users = new UserService().getUsers();
 
         User userFounded = users.stream()
-                .filter(user -> username.equals(user.getUsername()) && username.equals(user.getUsername()))
+                .filter(user -> username.equals(user.getUsername()) && password.equals(user.getPassword()))
                 .findFirst()
                 .orElse(null);
 
@@ -47,6 +43,5 @@ public class LogInServlet extends HttpServlet {
         }
     }
 
-    public void destroy() {
-    }
+    public void destroy() {}
 }
--- src/main/java/co/edu/unbosque/servletjsptutorial/dtos/User.java
+++ src/main/java/co/edu/unbosque/servletjsptutorial/dtos/User.java
@@ -16,6 +16,12 @@ public class User {
     public User() {
     }
 
+    public User(String username, String password, String role) {
+        this.username = username;
+        this.password = password;
+        this.role = role;
+    }
+
     public String getUsername() {
         return username;
     }
